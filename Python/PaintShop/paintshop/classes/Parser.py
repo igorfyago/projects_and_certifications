ca69c8f9580a54734c80393a61da508448619315
Igor F. Yago <igor.yago@gmail.com>
2018-10-14 12:18:28 +0100

    cleanup

--- Python/PaintShop/paintshop/classes/Parser.py
+++ Python/PaintShop/paintshop/classes/Parser.py
@@ -1,4 +1,4 @@
-def combine_every(line, n): return [line[i:i + n] for i in range(0, len(line), n)]
+def combine_as_list_every_n(line, n): return [line[i:i + n] for i in range(0, len(line), n)]
 
 
 def formatted(line): return line.rstrip('\n').replace(" ", "").replace("\t", "").strip()
@@ -19,7 +19,7 @@ class Parser:
 
     def add_customers(self):
         for row_id, line in enumerate(self.input_lines[1:]):
-            cust_prefs = dict([[int(string[0]), str(string[1])] for string in combine_every(line,2)])
+            cust_prefs = dict([[int(string[0]), str(string[1])] for string in combine_as_list_every_n(line,2)])
             Customer("cust_" + str(row_id + 1), cust_prefs, len(cust_prefs), self.path)
 
     def set_amount_requested_colors(self):
